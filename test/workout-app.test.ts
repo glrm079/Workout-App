43aa1ad9b519e20be2bf368e3bd51d68ad699e5b
glrm079 <glrm07928@gmail.com>
2026-02-24 19:06:09 -0300

    Initial commit

---
+++ test/workout-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WorkoutApp from '../lib/workout-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/workout-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WorkoutApp.WorkoutAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
